3013816c696d8d43a906e29564d53a4c442acfc6
ekomekoteko <45575162+ekomekoteko@users.noreply.github.com>
2019-01-18 04:06:12 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -658,7 +658,27 @@ msg.channel.send('🚬☁☁☁')
   msg.react('☁')
 }
 });
-
+client.on('message', function(msg) {
+    if(msg.content.startsWith (prefix + 'server')) {
+      if(!msg.channel.guild) return msg.reply('**:x: اسف لكن هذا الامر للسيرفرات فقط **');
+      let embed = new Discord.RichEmbed()
+      .setColor('RANDOM')
+      .setThumbnail(msg.guild.iconURL)
+      .addField(':globe_with_meridians: **اسم السيرفر : **' , `**[ ${msg.guild.name} ]**`,true)
+      .addField(':earth_africa: ** موقع السيرفر :**',`**[ ${msg.guild.region} ]**`,true)
+      .addField(':military_medal:** الرتب :**',`**[ ${msg.guild.roles.size} ]**`,true)
+      .addField(':bust_in_silhouette:** عدد الاعضاء :**',`**[ ${msg.guild.memberCount} ]**`,true)
+      .addField(':white_check_mark:** عدد الاعضاء الاونلاين :**',`**[ ${msg.guild.members.filter(m=>m.presence.status == 'online').size} ]**`,true)
+      .addField(':pencil:** الرومات الكتابية :**',`**[ ${msg.guild.channels.filter(m => m.type === 'text').size} ]**`,true)
+      .addField(':loud_sound:** رومات الصوت :**',`**[ ${msg.guild.channels.filter(m => m.type === 'voice').size} ]**`,true)
+      .addField(':crown:** صاحب السيرفر :**',`**[ ${msg.guild.owner} ]**`,true)
+      .addField(':id:** ايدي السيرفر :**',`**[ ${msg.guild.id} ]**`,true)
+      .addField(':date:** تم عمل السيرفر في : **',msg.guild.createdAt.toLocaleString())
+      .addField(':sleeping:** روم AFK**',`**${msg.guild.afkChannel || 'None'}**`, true)
+      .addField(':upside_down:** الايموجيات :**', `**${msg.guild.emojis.size}** \`[\` **${msg.guild.emojis.map(m => m).join('**|**')} \`]\`**`, true);
+      msg.channel.send({embed:embed});
+    }
+  });
 
 
 
